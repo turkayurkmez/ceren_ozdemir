2256ec02e5da7716c51d806c608659d24b616f15
türkay ürkmez <turkay.urkmez@outlook.com>
2025-04-09 21:11:09 +0300

    çoklu catch kullanımı

---
+++ IntroCsharp/useTryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    Console.WriteLine("İlk sayıyı giriniz:");
+    int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("İkinci sayıyı giriniz:");
+    int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+    int bolum = sayi1 / sayi2;
+    Console.WriteLine("Sonuç:" + bolum);
+
+}
+catch (FormatException ex) 
+{
+    Console.WriteLine("Lütfen geçerli bir sayı giriniz.");
+   
+}
+catch (DivideByZeroException ex2){
+    Console.WriteLine("Bir tam sayıyı sıfıra bölemezsiniz.");
+}
+catch (Exception ex){
+    Console.WriteLine("Bilinmeyen bir hata oluştu: " + ex.Message);
+}
+
+
